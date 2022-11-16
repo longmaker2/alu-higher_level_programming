5deb158d19094c508dcc9acde5aa405ae2671509
longmaker2 <l.deng@alustudent.com>
2022-11-16 08:01:16 -0800

    task 1 correced

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''a function that writes a string to a text 
-file (UTF8) and returns the number of characters written'''
+'''writes a string to a text file (UTF8) and
+returns the number of characters written'''
 
 
 def write_file(filename="", text=""):
