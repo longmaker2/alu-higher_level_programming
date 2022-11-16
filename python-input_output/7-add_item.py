c95058ce41c0791a9f16bd56c96692b07af85fa8
longmaker2 <l.deng@alustudent.com>
2022-11-16 08:41:10 -0800

    task 7 done

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a script that adds all arguments to a Python list, and then save them to a file"""
+"""script that adds all arguments to a Python list, and then save them to a file"""
 
 
 import sys
