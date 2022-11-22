74433948eaf39fc385e550b6656c9300902e206d
longmaker2 <l.deng@alustudent.com>
2022-11-22 19:08:58 +0200

    task two done

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+"""a Python script that displays the body of the response (decoded in utf-8)"""
 
 import urllib.request
 import urllib.parse
