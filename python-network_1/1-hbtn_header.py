0b4c93985e747799ab7bedc2ebf350597045c0d4
longmaker2 <l.deng@alustudent.com>
2022-11-22 18:41:50 +0200

    task 1 done

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""ends a request to the URL and displays the value of the X-Request-Id
-variable found in the header of the response"""
+'''value of the X-Request-Id variable '''
+
 
 import urllib.request
 import sys
 
+
 if __name__ == '__main__':
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        header = response.info()
-        print(header["X-Request-Id"])
+    with urllib.request.urlopen(sys.argv[1]) as reply:
+        header = reply.info()
+        print(header['X-Request-Id'])
