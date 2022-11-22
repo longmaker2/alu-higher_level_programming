6578f04a1baa311ff34cb70a6de8889446afb580
longmaker2 <l.deng@alustudent.com>
2022-11-22 19:12:43 +0200

    task one done

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response'''
+'''a Python script that displays the value of the X-Request-Id variable'''
 
 
 import urllib.request
