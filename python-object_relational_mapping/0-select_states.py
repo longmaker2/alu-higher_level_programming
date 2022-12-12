41452ec81293ffc11e8fb0d43ef90267d9bf8af4
longmaker2 <l.deng@alustudent.com>
2022-12-12 22:56:29 +0300

    updated files

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
+"""
+    Create a script that lists all states from a database hbtn_0e_0_usa
+"""
 
 import sys
 import MySQLdb
