043bf3b63ae7421f0cc966e3baa97d8f441adaa4
longmaker2 <l.deng@alustudent.com>
2022-12-13 01:21:38 +0300

    task 7 done

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''a python file that contains the class definition of
+a State and an instance Base = declarative_base()'''
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                        sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True
+                    )
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
