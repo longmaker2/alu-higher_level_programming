a638878ac6935939fa6397bd75a109e7fbc9b176
longmaker2 <l.deng@alustudent.com>
2023-02-01 00:24:17 +0300

    files updated

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+// a script that computes and prints a factorial
 function factorial (num) {
-    if (isNaN(num) || num <= 1) {
-      return 1;
-    }
+  if (isNaN(num) || num < 0) {
+    return 1;
+  } else if (num === 0) {
+    return 1;
+  } else {
     return num * factorial(num - 1);
   }
-  console.log(factorial(parseInt(process.argv[2])));
-  
+}
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
