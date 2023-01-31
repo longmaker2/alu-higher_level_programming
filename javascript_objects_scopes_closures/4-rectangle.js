4da40cef7df9b3e5561489ce31f4cb2c21403bc1
longmaker2 <l.deng@alustudent.com>
2023-02-01 01:45:10 +0300

    files updated

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  
+  rotate () {
+    const tempWidth = this.width;
+    this.width = this.height;
+    this.height = tempWidth;
+  }
+  
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
