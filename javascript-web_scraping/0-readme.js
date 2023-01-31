6a094603b329d5ac4eaa29e1f713d5fcf7304bf2
longmaker2 <l.deng@alustudent.com>
2023-02-01 00:09:10 +0300

    files updated

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-// a script that reads and prints the content of a file
 const fs = require('fs');
-
-fs.readFile(process.argv[2], (err, contents) => {
-  err ? console.log(err) : console.log(contents.toString());
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
