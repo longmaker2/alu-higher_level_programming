d7ed1c54336fe4e1340a024aa843079f37e2b9ad
longmaker2 <l.deng@alustudent.com>
2023-02-01 00:22:27 +0300

    files updated

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
+// a script that prints the addition of 2 integers
 function add (a, b) {
-    console.log(a + b);
-  }
-  add(parseInt(process.argv[2]), parseInt(process.argv[3]));
-  
+  console.log(a + b);
+}
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));
